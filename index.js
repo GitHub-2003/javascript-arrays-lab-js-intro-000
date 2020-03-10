763912512898b3224bdce83b62858dd314cc7b26
MJU <marie.ju2012@outlook.com>
2020-03-10 17:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 function destructivelyAppendKitten(name) {
-  
+
 }
 
 function destructivelyPrependKitten(name) {
-  
+
 }
 
 function destructivelyRemoveLastKitten() {
-  
+
 }
-
-
